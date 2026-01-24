5d58c61f56e1999928df5b1ccd24080d5a6dd355
Primitiva <heyprimitiva@gmail.com>
2026-01-24 14:52:29 -0300

    perf: advanced script optimization (preload, inlining, chunks) to minimize latency

--- astro.config.mjs
+++ astro.config.mjs
@@ -19,6 +19,7 @@ export default defineConfig({
     }),
     build: {
         inlineStylesheets: 'always',
+        modulePreload: { polyfill: false },
     },
     prefetch: {
         prefetchAll: true,
